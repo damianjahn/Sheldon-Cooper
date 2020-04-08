3d285c1f67a252274ec2d79e5b5ed0ccc12b8f57
Damian <damian.jahn@icloud.com>
2020-04-08 15:16:36 +0200

    Initial commit

---
+++ Sheldon Cooper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sheldon Cooper
+//
+//  Created by Damian Jahn on 08/04/2020.
+//  Copyright © 2020 Damian Jahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
